47a7dff2756fc0af1d628ee6d0be6a62d23be9f5
Steve Towner <stevetowner@yahoo.com>
2016-06-23 22:30:52 -0500

    Initial commit.

---
+++ Source/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Music_Genre_Grouper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileFolderTextbox1 = new FileFolderTextbox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CreateM3UCheckbox = new System.Windows.Forms.CheckBox();
+            this.ChangeGenreCheckbox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileFolderTextbox1
+            // 
+            this.fileFolderTextbox1.AbsolutePath = "H:\\Music";
+            this.fileFolderTextbox1.AllowFiles = false;
+            this.fileFolderTextbox1.AutoCompleteList = null;
+            this.fileFolderTextbox1.Label = "Select folder:";
+            this.fileFolderTextbox1.Location = new System.Drawing.Point(12, 12);
+            this.fileFolderTextbox1.Name = "fileFolderTextbox1";
+            this.fileFolderTextbox1.RecentItemsString = "H:|H:\\Music";
+            this.fileFolderTextbox1.RelativeBasePath = "";
+            this.fileFolderTextbox1.Size = new System.Drawing.Size(503, 21);
+            this.fileFolderTextbox1.TabIndex = 0;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(440, 340);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // objectListView1
+            // 
+            this.objectListView1.Location = new System.Drawing.Point(12, 39);
+            this.objectListView1.Name = "objectListView1";
+            this.objectListView1.Size = new System.Drawing.Size(503, 164);
+            this.objectListView1.TabIndex = 2;
+            this.objectListView1.UseCompatibleStateImageBehavior = false;
+            this.objectListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 209);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(503, 125);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // CreateM3UCheckbox
+            // 
+            this.CreateM3UCheckbox.AutoSize = true;
+            this.CreateM3UCheckbox.Checked = true;
+            this.CreateM3UCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CreateM3UCheckbox.Location = new System.Drawing.Point(14, 341);
+            this.CreateM3UCheckbox.Name = "CreateM3UCheckbox";
+            this.CreateM3UCheckbox.Size = new System.Drawing.Size(80, 17);
+            this.CreateM3UCheckbox.TabIndex = 4;
+            this.CreateM3UCheckbox.Text = "Create m3u";
+            this.CreateM3UCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // ChangeGenreCheckbox
+            // 
+            this.ChangeGenreCheckbox.AutoSize = true;
+            this.ChangeGenreCheckbox.Location = new System.Drawing.Point(126, 340);
+            this.ChangeGenreCheckbox.Name = "ChangeGenreCheckbox";
+            this.ChangeGenreCheckbox.Size = new System.Drawing.Size(93, 17);
+            this.ChangeGenreCheckbox.TabIndex = 5;
+            this.ChangeGenreCheckbox.Text = "Change genre";
+            this.ChangeGenreCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 375);
+            this.Controls.Add(this.ChangeGenreCheckbox);
+            this.Controls.Add(this.CreateM3UCheckbox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.objectListView1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.fileFolderTextbox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FileFolderTextbox fileFolderTextbox1;
+        private System.Windows.Forms.Button StartButton;
+        private BrightIdeasSoftware.ObjectListView objectListView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox CreateM3UCheckbox;
+        private System.Windows.Forms.CheckBox ChangeGenreCheckbox;
+    }
+}
+
---
+++ Source/Form1.cs
@@ -0,0 +1,111 @@
+﻿namespace Music_Genre_Grouper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        #region Fields
+
+        FolderWalker folderWalker;
+        SortedDictionary<string, string> genreGroups;
+        SortedDictionary<string, List<string>> filepathsByGenreGroup;
+        List<string> unmatchedGenres;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Event Handlers
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            genreGroups = new SortedDictionary<string, string>();
+            foreach (var item in textBox1.Lines)
+            {
+                string genre = item.Substring(item.IndexOf(':') + 1).Trim();
+                string group = item.Substring(0, item.IndexOf(':'));
+
+                if (!genreGroups.ContainsKey(genre))
+                    genreGroups.Add(genre, group);
+            }
+
+            folderWalker = new FolderWalker(fileFolderTextbox1.AbsolutePath, true);
+            folderWalker.Finished += FolderWalker_Finished;
+            folderWalker.Start();
+        }
+
+        #endregion Event Handlers
+
+        #region Methods
+
+        private void FolderWalker_Finished(List<System.IO.DirectoryInfo> ProcessedFolders, List<System.IO.FileInfo> ProcessedFiles)
+        {
+            unmatchedGenres = new List<string>();
+            filepathsByGenreGroup = new SortedDictionary<string, List<string>>();
+            foreach (var musicFile in ProcessedFiles)
+            {
+                if (musicFile.Extension.Equals(".mp3", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    TagLib.File tagFile = TagLib.File.Create(musicFile.FullName);
+                    foreach (var genre in tagFile.Tag.Genres)
+                    {
+                        if (genreGroups.ContainsKey(genre))
+                        {
+                            string genreGroup = genreGroups[genre];
+
+                            if (filepathsByGenreGroup.ContainsKey(genreGroup))
+                                filepathsByGenreGroup[genreGroup].Add(musicFile.FullName);
+                            else
+                                filepathsByGenreGroup.Add(genreGroup, new List<string>());
+
+                            if (ChangeGenreCheckbox.Checked)
+                            {
+                                //tagFile.Save();
+                            }
+                        }
+                        else
+                        {
+                            unmatchedGenres.Add(genre);
+                        }
+                    }
+                }
+            }
+
+            // Save each m3u playlist
+            if (CreateM3UCheckbox.Checked)
+            {
+                foreach (var genreGroup in filepathsByGenreGroup)
+                {
+                    List<string> relativePaths = new List<string>();
+                    foreach (var filepath in genreGroup.Value)
+                    {
+                        string relativePath = PathFunctions.RelativePath(fileFolderTextbox1.AbsolutePath,filepath);
+                        relativePaths.Add(relativePath);
+                    }
+
+                    File.WriteAllLines(Path.Combine(fileFolderTextbox1.AbsolutePath, genreGroup.Key + ".m3u"), relativePaths.ToArray());
+                }
+            }
+
+            MessageBox.Show("Finished.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #endregion Methods
+    }
+}
